cc2827518fa39917f10782aff7a5ec4c0a0c3b88
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-30 01:06:35 +0600

    update

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div style={{textAlign: 'center', margin: '30px'}}>
+           <h1>This is an error, you can try another way</h1> 
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/Components/Home/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+          <h4>Footer part</h4>  
+        </div>
+    );
+};
+
+export default Footer;
