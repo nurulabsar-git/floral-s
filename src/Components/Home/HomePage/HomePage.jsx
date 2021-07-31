f45ffd3365a0441ebb82defb3c5f48bd4a51e2ba
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-31 12:18:53 +0600

    nuru

--- src/Components/Home/HomePage/HomePage.jsx
+++ src/Components/Home/HomePage/HomePage.jsx
@@ -3,10 +3,10 @@ import './HomePage.css';
 import bg from '../../image/porte-sainte-claire-3.png';
 const HomePage = () => {
     return (
-        <div className="home-container text-center" >
-            <div className="p-56 text-center">
-            <input type="text" style={{width: '280px', height: '35px'}} />
-            </div>
+        <div className="home-container text-center pt-56" >
+
+            <input type="text" placeholder="Search" style={{width: '280px', height: '35px', padding: '5px'}} />
+            <button className="bg-blue-900 h-10 w-10"><i class="fas fa-search"></i></button>
         </div>
     );
 };
