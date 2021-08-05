6c440844ca55b9e9f7265b0d6a15f38195b4f27d
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-04 23:56:11 -0700

    update

--- src/Components/Home/HomePage/HomePage.jsx
+++ src/Components/Home/HomePage/HomePage.jsx
@@ -6,12 +6,7 @@ const HomePage = () => {
         <div className="home-container text-center pt-56" >
             <input type="text" placeholder="Search" style={{width: '280px', height: '35px', padding: '5px'}} />
             <button className="bg-blue-900 h-10 w-10"><i class="fas fa-search"></i></button>
-
-        <div className="home-container text-center" >
-            <div className="p-56 text-center">
-            <input type="text" style={{width: '280px', height: '35px'}} />
-            </div> 
-        </div>
+       </div>
     );
 };
 
